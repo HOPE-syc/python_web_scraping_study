29c2230db1db4a52ea31d15a61956e6a6ad8e9a2
HOPE-syc <yc4332@naver.com>
2023-05-28 23:25:24 +0900

    Update main.py
    좀 더 수정보완함.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("웹 사이트에 접근할 수 없습니다.")
 else:
+    results = []
     soup = BeautifulSoup(response.text, "html.parser")
     book_info = soup.find_all("div", class_="prod_info_box")
     for book_info_name_section in book_info:
@@ -16,14 +17,23 @@ else:
         book_link_info = book_links[0]
         book_link_info_link = book_link_info['href']
         book_author_box = book_info_name_section.find_all('div', class_="auto_overflow_inner")
-        book_price_main = book_info_name_section.find_all('span', class_="price")
+        book_price_main = book_info_name_section.find_all('span', class_="val")
         book_price = book_price_main[0]
         for book_author_section in book_author_box:
             book_authors = book_author_section.find('a', class_="author")
             for book_link in book_links:    
                 book_names = book_link.text
                 if book_authors is not None:
-                    print(book_link_info_link,book_names,book_authors.text,book_price.text)
+                    book_data = {
+                        'book_link' : book_link_info_link,
+                        'book_names' : book_names,
+                        'book_authors' : book_authors.string,
+                        'book_price' : book_price.string
+                    }
+                    results.append(book_data)
 
+    for result in results:
+        print(result)
+        print("///////////////")
                 
            
