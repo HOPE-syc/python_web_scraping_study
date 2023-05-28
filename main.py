272ab65c68e053ce6fe06679450c0388cfb644b1
HOPE-syc <yc4332@naver.com>
2023-05-28 23:02:21 +0900

    Update main.py
    검색기능 확대와 코드문법과 맥락을 다듬고 공부함.

--- main.py
+++ main.py
@@ -7,21 +7,23 @@ search_term = "파이썬"
 response = get(f"{base_url}{search_term}")
 
 if response.status_code != 200:
-    print("웹 사이트를 막았습니다.")
+    print("웹 사이트에 접근할 수 없습니다.")
 else:
     soup = BeautifulSoup(response.text, "html.parser")
     book_info = soup.find_all("div", class_="prod_info_box")
-    print(book_info)
-    """
-    book_info_name = soup.find_all("a", class_="prod_info"), soup.find_all("a", class_="author rep")
-    """
     for book_info_name_section in book_info:
         book_links = book_info_name_section.find_all("a", class_="prod_info")
         book_link_info = book_links[0]
         book_link_info_link = book_link_info['href']
-        book_author = book_info_name_section.find('a', class_="author rep")
-        print(book_author.text)
-        for book_link in book_links:    
-            book_names = book_link.find("span")
-            # print(book_link_info_link, book_names.string)
+        book_author_box = book_info_name_section.find_all('div', class_="auto_overflow_inner")
+        book_price_main = book_info_name_section.find_all('span', class_="price")
+        book_price = book_price_main[0]
+        for book_author_section in book_author_box:
+            book_authors = book_author_section.find('a', class_="author")
+            for book_link in book_links:    
+                book_names = book_link.text
+                if book_authors is not None:
+                    print(book_link_info_link,book_names,book_authors.text,book_price.text)
+
+                
            
